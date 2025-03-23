3805394559540eef087815756e79adbda6cfe03e
Wateir <comptesbidon.fr@gmail.com>
2025-03-23 18:37:50 +0100

    Start of display

--- V1.py
+++ V1.py
@@ -17,18 +17,24 @@ def donner_grille():
 def mettre_char_coord(grille,x,y,char):
     grille[x][y]=char
 
-def initialise(grille):
-    for y in range(3):
-        for i in range(TAILLE_GRILLE):
+def initialise_debut(grille):
+    i = 0
+    for i in range(int(TAILLE_GRILLE/3)):
+        for y in range(TAILLE_GRILLE):
             mettre_char_coord(grille, i, y, "●")
+    for i in range(int(TAILLE_GRILLE/3+3),TAILLE_GRILLE):
+        for y in range(TAILLE_GRILLE):
+            mettre_char_coord(grille, i , y, "○")
 
-    for y2 in range(3):
-        for i in range(TAILLE_GRILLE):
-            mettre_char_coord(grille, i, y2 + 3, "○")
+def initialise_milieu(grille):
+    pass
+
+def initialise_fin(grille):
+    pass
 
 def afficher_grille(matrice):
-    print("    A   B   C   D   E   F   G   H   I   J  ")
-    print("  -----------------------------------------")
+    print("    A   B   C   D   E   F   G   H   I  ")
+    print("  -------------------------------------")
     coordonnée_vertical = 0
     for ligne in matrice:
         ligne_final = "|"
@@ -36,7 +42,7 @@ def afficher_grille(matrice):
         for valeur in ligne:
             ligne_final += f" {valeur} |"
         print(coordonnée_vertical, ligne_final)
-        print("  -----------------------------------------")
+        print("  -------------------------------------")
 
 
 
@@ -57,14 +63,14 @@ def est_dans_grille(coord):
     if not est_au_bon_format(coord):
         return 0
 
-    lettre = transfrom_lettre_chiffre("chiffre",coord[0])
+    lettre = transfrom_lettre_chiffre(coord[0])
     chiffre = int(coord[1]) - 1
     if lettre > 8 or chiffre < 0 or chiffre > 8:
         return 0
 
     return 1
 
-def transfrom_lettre_chiffre(type,chiffre):
+def transfrom_lettre_chiffre(chiffre):
     lettres = {
         "A": 0,
         "B": 1,
@@ -112,10 +118,23 @@ def test_est_dans_grille():
     assert est_dans_grille("A0") == 0  , "Pas dans la grille"
     assert est_dans_grille("J9") == 0  , "First case not on the grille"
 
-test_est_au_bon_format()
-test_est_dans_grille()
 
-plateau_jeu = donner_grille()
+def test():
+    test_est_au_bon_format()
+    test_est_dans_grille()
 
-initialise(plateau_jeu)
-afficher_grille(plateau_jeu)
+def lancement():
+    plateau_jeu = donner_grille()
+
+
+    print(" ____             _        ")
+    print("| __ )  ___  __ _| |_ __ _ ")
+    print("|  _ \\ / _ \\/ _` | __/ _` |")
+    print("| |_) |  __/ (_| |  ||(_| |")
+    print("|____/ \\___|\\__,_|\\__\\__,_|")
+    print("")
+
+    initialise_debut(plateau_jeu)
+    afficher_grille(plateau_jeu)
+
+lancement()
