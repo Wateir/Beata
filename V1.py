d566a0738c8043d399053b8e9baa7e9adb5d14e8
Wateir <comptesbidon.fr@gmail.com>
2025-03-23 23:31:05 +0100

    End display
    no more thing to add on dispaly for Atelier 1

--- V1.py
+++ V1.py
@@ -1,5 +1,11 @@
 TAILLE_GRILLE = 9
 
+def error(texte,type):
+    if type == 0:
+        print("\033[31m==>\033[0m "+ texte)
+        print("")
+
+
 def donner_grille():
     grille = [
         [" "," "," "," "," "," "," "," "," "],
@@ -13,6 +19,13 @@ def donner_grille():
         [" "," "," "," "," "," "," "," "," "],
     ]
     return grille
+
+def convertir_en_tuple(coord):
+    lettre = coord[0].upper()
+    chiffre = int(coord[1])
+    lettre_num = ord(lettre) - ord('A')
+    return (lettre_num, chiffre - 1)
+
 
 def mettre_char_coord(grille,x,y,char):
     grille[x][y]=char
@@ -48,64 +61,70 @@ def afficher_grille(matrice):
 
 
 def est_au_bon_format(coord):
-    if len(coord) == 2:
-        if coord[0].isalpha and coord[0].isupper:
-            if coord[1].isdigit:
-                return 1
-            else:
-                return 0
-        else:
-            return 0
-    else:
-        return 0
+    if len(coord) == 2 and coord[0].isalpha() and coord[1].isdigit():
+        lettre = coord[0].upper()
+        chiffre = int(coord[1])
+        if lettre in 'ABCDEFGHI' and 1 <= chiffre <= 9:
+            return True
+    return False
 
 def est_dans_grille(coord):
-    if not est_au_bon_format(coord):
+    if est_au_bon_format(coord):
+        coord_tuple = convertir_en_tuple(coord)
+        if coord_tuple:
+            x, y = coord_tuple
+            if 0 <= x <= 8 and 0 <= y <= 8:
+                return True
+            return False
+    else:
+        return False
+
+
+
+
+def menu_choix(type):
+    print("Select a mode :")
+    possibilité = "()"
+    test_pos = ["1"]
+
+
+    if type == "ouverture":
+        print(" -> 1 : Atelier 1")
+        print(" -> 2 : Jouer au jeu (Pas implementer)")
+        print(" -> 3 : Relancer les tests")
+        possibilité = "(1-3)"
+        test_pos = ["1","2","3"]
+
+    if type == "Atelier1":
+        print(" -> 1 : Configuration de depart")
+        print(" -> 2 : Configuration du milieu")
+        print(" -> 3 : Configuration de fin")
+        print(" -> 4 : Rentrer une coordonnée")
+        possibilité = "(1-4)"
+        test_pos = ["1","2","3","4"]
+
+    if type == "Atelier12":
+        print(" -> 1 : Configuration de depart")
+        print(" -> 2 : Configuration du milieu")
+        print(" -> 3 : Configuration de fin")
+        print(" -> 4 : Rentrer une coordonnée")
+        print(" -> 5 : Quitter")
+        possibilité = "(1-5)"
+        test_pos = ["1","2","3","4","5"]
+
+    print("")
+    choix = input("Entrer un numero "+ possibilité +" : ")
+    if choix in test_pos:
+        return int(choix)
+    else:
         return 0
-
-    lettre = transfrom_lettre_chiffre(coord[0])
-    chiffre = int(coord[1]) - 1
-    if lettre > 8 or chiffre < 0 or chiffre > 8:
-        return 0
-
-    return 1
-
-def transfrom_lettre_chiffre(chiffre):
-    lettres = {
-        "A": 0,
-        "B": 1,
-        "C": 2,
-        "D": 3,
-        "E": 4,
-        "F": 5,
-        "G": 6,
-        "H": 7,
-        "I": 8,
-        "J": 9,
-        "K": 10,
-        "L": 11,
-        "M": 12,
-        "N": 13,
-        "O": 14,
-        "P": 15,
-        "Q": 16,
-        "R": 17,
-        "S": 18,
-        "T": 19,
-        "U": 20,
-        "V": 21,
-        "W": 22,
-        "X": 23,
-        "Y": 24,
-        "Z": 25
-    }
-    return lettres[chiffre]
 
 
 def test_est_au_bon_format():
     assert est_au_bon_format("A1") == 1  , "First Case"
     assert est_au_bon_format("I9") == 1  ,"Last Case"
     assert est_au_bon_format("Bad") == 0 , "Bad"
+    assert est_au_bon_format("AZ") == 0  , "double lettre"
 
 
 
@@ -134,7 +153,47 @@ def lancement():
     print("|____/ \\___|\\__,_|\\__\\__,_|")
     print("")
 
-    initialise_debut(plateau_jeu)
-    afficher_grille(plateau_jeu)
+    test()
+
+    choix = 0
+    while not choix in [1]:
+        choix = menu_choix("ouverture")
+        if choix == 0:
+            error("Choix hors de porté",0)
+        if choix == 1:
+            print("Run test")
+            test()
+    choix = 0
+
+    choix = menu_choix("Atelier1")
+    while True:
+        if choix in [1,2,3,4]:
+            if choix == 1:
+                initialise_debut(plateau_jeu)
+                afficher_grille(plateau_jeu)
+
+            elif choix == 2:
+                initialise_milieu(plateau_jeu)
+                afficher_grille(plateau_jeu)
+            elif choix == 3:
+                initialise_fin(plateau_jeu)
+                afficher_grille(plateau_jeu)
+            elif choix == 4:
+                coord = input("Rentrer une coordonnée (exp : A3) : ")
+                if est_au_bon_format(coord):
+                    if not est_dans_grille(coord):
+                        print("Coordonnée invalide")
+                    else:
+                        x, y = convertir_en_tuple(coord)
+                        mettre_char_coord(plateau_jeu, x, y, "X")
+                        print("Yo")
+                        afficher_grille(plateau_jeu)
+                else:
+                    print("Coordonnée invalide")
+        elif choix == 5:
+            break
+        choix = menu_choix("Atelier12")
+
+
 
 lancement()
